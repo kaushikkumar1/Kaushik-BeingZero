624ae441eedc99a3f1669af524921b1521cb680f
kaushik <kaushikkumar004@gmail.com>
2019-12-19 15:46:05 +0530

    creating data server using json

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
 
+var obj={}
+obj.name = "Being Zero";
+obj.college = "CMRCET";
+obj.regno = "111111ABC";
+
+app.get('/data', (req, res) => res.json(obj))
+
+app.get('/abt', (req, res) => res.sendFile(__dirname + '/public/htmls/abt.html'))
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
